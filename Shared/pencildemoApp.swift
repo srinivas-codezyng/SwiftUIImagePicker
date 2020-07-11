1505a2f492234cf289fe0781198519b6e99b7c30
Srinivas Prabhu <srinivas@codezyng.com>
2020-07-11 14:26:14 +0530

    Initial Commit

---
+++ Shared/pencildemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pencildemoApp.swift
+//  Shared
+//
+//  Created by Srinivas Prabhu on 11/07/20.
+//
+
+import SwiftUI
+
+@main
+struct pencildemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
